98db6c4bc68ce61aad8c182bf99bca551bb46409
Muhammad Richie Hadiansah <mrichieh123@gmail.com>
2025-11-03 18:53:53 +0700

    Variabel Arra dan Form Tambah Produk

--- routes/web.php
+++ routes/web.php
@@ -7,22 +7,44 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+// Route::get('/about', function () {
+//     return view('about');
+// });
 
 Route::get('/produk', function () {
     return view('produk');
 });
 
+Route::get('/produk', function () {
+    $page_title = 'Produk';
+
+    $products = [
+        [
+            "kode_produk" => "BRG001",
+            "nama_produk" => "Pena",
+            "jenis_produk" => "Alat Tulis",
+            "harga_produk" => 20000
+        ],
+        [
+            "kode_produk" => "BRG002",
+            "nama_produk" => "Buku",
+            "jenis_produk" => "Alat Tulis",
+            "harga_produk" => 15000
+        ]
+    ];
+
+    return view('produk', compact('page_title', 'products'));
+});
+
+
 // Route::get('/mahasiswa', function () {
 //     return view('mahasiswa');
 // });
 
-Route::resource('mahasiswa', MahasiswaController::class);
+// Route::resource('mahasiswa', MahasiswaController::class);
 
-Route::get('/profile', function () {
-    $nama = 'Muhammad Richie Hadiansah';
-    // return view('profile', compact('nama'));
-    return view('profile')->with('nama', $nama);
-});
+// Route::get('/profile', function () {
+//     $nama = 'Muhammad Richie Hadiansah';
+//     // return view('profile', compact('nama'));
+//     return view('profile')->with('nama', $nama);
+// });
