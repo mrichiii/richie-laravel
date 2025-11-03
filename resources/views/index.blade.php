09583342f8fb7b71c59fc3599deae33dc387e3cd
Muhammad Richie Hadiansah <mrichieh123@gmail.com>
2025-11-03 17:32:24 +0700

    Sidebar Bootstrap 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,39 +9,41 @@
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar bg-body-tertiary fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">UTS Laravel</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">UTS Laravel</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
+                <a class="nav-link" href="#">Produk</a>
             </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/profile">Profile</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="/mahasiswa">Mahasiswa</a>
-            </li>
-        </ul>
-        <form class="d-flex" role="search">
+            </ul>
+            <form class="d-flex mt-3" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
             <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+            </form>
+        </div>
         </div>
     </div>
+        <div class="container">
+            <h1>Halaman Home</h1>
+            <br>
+        </div>
     </nav>
-    <div class="container">
-    <h1>Ini halaman Home</h1>
-    <br>
-    </div>
+    
 
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
